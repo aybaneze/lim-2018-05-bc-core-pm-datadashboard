a3f17d7bc5d1f1f6ff55b03aca209e42c6658597
leydy <leydymayumy@gmail.com>
2018-06-27 22:22:19 -0500

    subiendo select

--- src/data.js
+++ src/data.js
@@ -1,90 +1,96 @@
+let users = {};
+let progress = {};
+let courses = {};
 
-window.onload = function () {
-var computeUsersStats= ()=>{//users, progress, courses)=>{
-    fetch('http://127.0.0.1:8080/data/cohorts.json')
-    .then((response)=>{
-       return response.json();
-    }).then((jsonData)=>{
-		debugger;
-		
-		var Table = document.getElementById("TableBody");
-		Table.innerHTML = "";
-		
-		for(let i = 0 ; i<jsonData.length;i++){
-			debugger;
-			let anio = jsonData[i].start.substring(0, 4);
-			var primerSegmentoCohort = jsonData[i].id.split("-");
-			let cohort = primerSegmentoCohort[0];
-			debugger;
-			if(cohort === cohortsSeleccionado && anio === anioSeleccionado ){
-				debugger;
-				let TableRow="<tr></tr>";
-               TableRow = "<td>" + jsonData[i].id + "</td>"+
-                          "<td>" + jsonData[i].usersCount+ "</td>"+
-                          "<td>" + jsonData[i].start + "</td>";
-						  
-			   var TrElement = document.createElement("tr");
-               TrElement.innerHTML = TableRow;
-               document.getElementById("TableBody").appendChild(TrElement);
-			   debugger;
-			}
-			
-			
-			
-		}
-    
-      /*for(let x=0;x<jsonData.length;x++){
-             let TableRow="<tr></tr>";
-               TableRow = "<td>" + jsonData[x].name + "</td>"+
-                          "<td>" + jsonData[x].id+ "</td>"+
-                          "<td>" + jsonData[x].role + "</td>";
-           
-             var TrElement = document.createElement("tr");
-               TrElement.innerHTML = TableRow;
-               document.getElementById("TableBody").appendChild(TrElement);
-    
-       console.log(jsonData[0]);
-		}	*/
-	});
-     
-    };
+fetch('https://api.laboratoria.la/cohorts')
+  .then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      console.log('ocurrio error')
+    }
+  }).then((jsonDataCohort) => {
+    courses = jsonDataCohort;
+  })
 
-/*const data= new XMLHttpRequest();
-data.open('GET','http://127.0.0.1:8080/data/cohorts.json',true);
-data.onload=add;
-data.send();
-console.log(data);
+fetch('https://leydy.github.io/lim-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json')
+  .then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      console.log('ocurrio error')
+    }
+  }).then((jsonDataUsers) => {
+    users = jsonDataUsers;
+    return fetch('https://leydy.github.io/lim-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json')
+  }).then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      console.log('ocurrio error')
+    }
+  }).then((jsonDataProgress) => {
+    progress = jsonDataProgress;
+    cargoData(users, progress)
+  })
+
+const cargoData = (users, progress) => {
+  console.log(users, progress);
 }
-
-function add () {
-
- const datos=JSON.parse(this.responseText);
- console.log(datos);
-}*/
-
-var lim=document.getElementById('lim');
-lim.addEventListener('click',computeUsersStats);
-
 
 
 var grupoAnio = document.getElementById("conjuntoAnio"); /*Obtener el SELECT */
 var anioSeleccionado = grupoAnio.options[grupoAnio.selectedIndex].value;
 
-var grupoCohort = document.getElementById("conjuntoBootcamp"); /*Obtener el SELECT */
+var grupoCohort = document.getElementById("conjuntoSede"); /*Obtener el SELECT */
 var cohortsSeleccionado = grupoCohort.options[grupoCohort.selectedIndex].value;
 
-function ShowSelected1()
-{
-/* Para obtener el valor */
-anioSeleccionado= document.getElementById("conjuntoAnio").value;
+var grupoCohortCombo = document.getElementById("conjuntoBootcamp"); /*Obtener el SELECT */
+var cohortsSeleccionadoCombo = grupoCohortCombo.options[grupoCohortCombo.selectedIndex].value;
+
+//obtiene lo seleccion en el select conjuntoAnio
+function ShowSelected1() {
+  /* Para obtener el valor */
+
+  anioSeleccionado = document.getElementById("conjuntoAnio").value;
+
+}
+//obtiene lo seleccion en el select selectSede
+function selectSede() {
+  /* Para obtener el valor */
+  debugger;
+  cohortsSeleccionado = document.getElementById("selectSede").value;
+  //obtiene lo seleccion en el select conjuntoBootcamp
+  debugger;
+  select = document.getElementById('conjuntoBootcamp');
+  select.innerHTML = "";
+  if (courses != undefined) {
+    for (let i = 0; i < courses.length; i++) {
+
+      let anio = courses[i].start.substring(0, 4);
+      var primerSegmentoCohort = courses[i].id.split("-");
+      let cohort = primerSegmentoCohort[0];
+
+      if (cohort === cohortsSeleccionado && anio === anioSeleccionado) {
+        debugger;
+        var opt = document.createElement('option');
+        opt.value = courses[i].id;
+        opt.innerHTML = courses[i].id;
+        select.appendChild(opt);
+
+
+      }
+    }
+  }
+}
+//obtiene lo seleccion en el select conjuntoBootcamp
+function selectBootcamp() {
+  /* Para obtener el valor */
+
+  cohortsSeleccionadoCombo = document.getElementById("conjuntoBootcamp").value;
 
 }
 
-function ShowSelected2()
-{
-/* Para obtener el valor */
-cohortsSeleccionado= document.getElementById("conjuntoBootcamp").value;
-
-}
+window.processCohortData = () => {
 }
 
